573626592ce717f757a7d3473711144cef59ff17
chritania <chritaniap@gmail.com>
2024-07-03 14:46:24 +0800

    INITIAL COMMIT

---
+++ Appchara/src/main/java/com/Appchara/Appchara/AppcharaApplication.java
@@ -0,0 +1,13 @@
+package com.Appchara.Appchara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcharaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcharaApplication.class, args);
+	}
+
+}
---
+++ Appchara/src/test/java/com/Appchara/Appchara/AppcharaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Appchara.Appchara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcharaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
