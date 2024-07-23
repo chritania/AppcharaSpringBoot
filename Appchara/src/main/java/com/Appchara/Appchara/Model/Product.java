9c7dca29d66f7e1b6fd11fc07a4e6e898676acb3
chritania <chritaniap@gmail.com>
2024-07-23 14:30:33 +0800

    fixing setters and getters

--- Appchara/src/main/java/com/Appchara/Appchara/Model/Product.java
+++ Appchara/src/main/java/com/Appchara/Appchara/Model/Product.java
@@ -23,15 +23,29 @@ public class Product {
         this.price = price;
     }
 
-
-    }
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-
+    //setters
     public void setProductId(Long productId) {
         this.productId = productId;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //getters
+    public Long getProductId() {
+        return productId;
     }
 
 
@@ -39,5 +53,13 @@ public class Product {
         return name;
     }
 
-    } 
-}
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public double getPrice() {
+        return price;
+    }
+} 
